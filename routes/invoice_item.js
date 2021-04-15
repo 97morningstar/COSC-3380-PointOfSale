4f2b83967e576026ec039f09d22f0f82ba2a31dc
JordanYu21 <67615449+JordanYu21@users.noreply.github.com>
2021-04-15 16:32:56 -0500

    Update invoice_item.js

--- routes/invoice_item.js
+++ routes/invoice_item.js
@@ -1,22 +1,15 @@
 const app = require('express').Router();
 const pool = require("../services/db")
 
-/*invoice_item
-    - update (quantity)
-    - delete [customer deletes an item from cart]
-    - get all by invoice_id = ? 
-    -combine with the invoice gets to give full transaction history and cart
-    - create a new invoice_item   [you need: quantity, item_id_fk]
-    */
 
-//  - update (quantity)
-// RESULT - SUCCESS
-app.put("/api/invoice_item/:id", async (req, res) => {
+
+//update quantity 
+app.put("/invoice_item/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const data = req.body;
 
-    const update_quantity = await pool.query("UPDATE invoice_item SET quantity = ? WHERE invoice_item_id = ? ",
+    const updateQuantity = await pool.query("UPDATE invoice_item SET quantity = ? WHERE invoice_item_id = ? ",
       [
         data.quantity,
         id
@@ -29,14 +22,13 @@ app.put("/api/invoice_item/:id", async (req, res) => {
 });
 
 
-//- get all by invoice_id = ? 
-// RESULT - SUCCESS
-app.get("/api/invoice_item/:id", async (req, res) => {
+//get invoice_item by invoice_item_id
+app.get("/invoice_item_hi/:id", async (req, res) => {
   try {
 
     const { id } = req.params;
 
-    const allInvoiceitem = await pool.query("SELECT * FROM invoice_item WHERE invoice_id = ?", [id]);
+    const allInvoiceitem = await pool.query("SELECT * FROM invoice_item WHERE invoice_item_id = ?", [id]);
 
     res.json(allInvoiceitem);
   } catch (err) {
@@ -44,55 +36,47 @@ app.get("/api/invoice_item/:id", async (req, res) => {
   }
 });
 
-//- delete [customer deletes an item from cart]
-// RESULT - SUCCESS
+//Delete invoice item by id
 app.delete("/api/invoice_item/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const deleteCustomer = await pool.query("DELETE FROM invoice_item WHERE invoice_item_id = ?", [id]);
+    const updateQuantity = await pool.query("UPDATE invoice_item SET quantity = 0");
+    const deleteInvoiceitem = await pool.query("DELETE FROM invoice_item WHERE invoice_item_id = ?", [id]);
     res.json("item was deleted successfully");
   } catch (err) {
     console.log(err.message);
   }
 });
 
+//Create new invoice_item 
 
-//- create a new invoice_item
-// RESULT: LOAD TIME (FAILED i guess...)
-app.post("/api/invoice_item", async (req, res) => {
+app.post("/create_invoice_item", async (req, res) => {
   try {
     const data = req.body;
     console.log(data);
-    const newInvoiceitem = await pool.query("INSERT INTO invoice_item(invoice_item_id, quantity, total_cost, item_id_fk , invoice_id_fk) VALUES ( ?, ?, ?, ?, ?)",
-      [data.invoice_item_id,
-      data.quantity,
-      data.total_cost,
-      data.item_id_fk,
-      data.invoice_id_fk,
-      ]);
-
-    res.json("An item was sucessfully added to your cart !");
+    // const checkItem = await pool.query("SELECT * FROM invoice_item WHERE invoice_id_fk = ? AND item_id_fk = ?", [data.invoice_id_fk, data.item_id_fk]);
+    //console.log(checkItem.length);
+    /*
+    if (checkItem.length != 0) {
+      const updateInvoiceitem = await pool.query("UPDATE invoice_item SET quantity = ? WHERE invoice_id_fk = ? AND item_id_fk = ?", [data.quantity, data.invoice_id_fk, data.item_id_fk]);
+      res.json("An invoice_item was been successfully updated.");
+    }
+    else {
+      const newInvoiceitem = await pool.query("INSERT INTO invoice_item( quantity, item_id_fk, invoice_id_fk) VALUES ( ?, ?, ?)",
+        [,
+          data.quantity,
+          data.item_id_fk,
+          data.invoice_id_fk
+        ]);
+      res.json("A new invoice_item was been successfully added.");
+      
+    }
+    */
   } catch (err) {
     console.log(err.message);
   }
 });
 
-
-
-//- get all cart items
-// RESULT: LOAD TIME (FAILED i guess...)
-app.get("/api/cart_invoice/", async (req, res) => {
-  try {
-
-    const { id } = req.params;
-
-    const allInvoiceitem = await pool.query("SELECT * FROM invoice_item WHERE order_status = 'cart'");
-
-    res.json(allInvoiceitem);
-  } catch (err) {
-    console.log(err.message);
-  }
-});
 
 
 module.exports = app;
